052e4259b1f74d07d0ceb5f42b1765333c88fb56
raheemadamboev <vividricemaryandmax@gmail.com>
2023-11-14 19:10:38 +0500

    Implemented zoom and rotation, movement;

--- app/src/main/java/xyz/teamgravity/zoommoveimage/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/zoommoveimage/MainActivity.kt
@@ -3,13 +3,22 @@ package xyz.teamgravity.zoommoveimage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import xyz.teamgravity.zoommoveimage.ui.theme.ZoomMoveImageTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,10 +26,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ZoomMoveImageTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                var scale by remember { mutableFloatStateOf(1F) }
+                var rotation by remember { mutableFloatStateOf(1F) }
+                var offset by remember { mutableStateOf(Offset.Zero) }
+
+                BoxWithConstraints(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(736F / 451F)
                 ) {
+                    val state = rememberTransformableState(
+                        onTransformation = { zoomChange, panChange, rotationChange ->
+                            scale = (scale * zoomChange).coerceIn(
+                                minimumValue = 1F,
+                                maximumValue = 5F
+                            )
+
+                            rotation += rotationChange
+
+                            val extraWidth = (scale - 1) * constraints.maxWidth
+                            val extraHeight = (scale - 1) * constraints.maxHeight
+
+                            val maxX = extraWidth / 2
+                            val maxY = extraHeight / 2
+
+                            offset = Offset(
+                                x = (offset.x + scale * panChange.x).coerceIn(
+                                    minimumValue = -maxX,
+                                    maximumValue = maxX
+                                ),
+                                y = (offset.y + scale * panChange.y).coerceIn(
+                                    minimumValue = -maxY,
+                                    maximumValue = maxY
+                                )
+                            )
+                        }
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.image),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .graphicsLayer {
+                                scaleX = scale
+                                scaleY = scale
+                                rotationZ = rotation
+                                translationX = offset.x
+                                translationY = offset.y
+                            }
+                            .transformable(state)
+                    )
                 }
             }
         }
